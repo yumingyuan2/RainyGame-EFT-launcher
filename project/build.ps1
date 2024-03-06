17c13650d01c2125a0f488c1f4b21675e967b2ff
Refringe <me@refringe.com>
2024-03-05 21:38:30 -0500

    Build Quotes & Specify Encoding of License

--- project/build.ps1
+++ project/build.ps1
@@ -5,18 +5,18 @@ $launcherAssetFolder = "..\Aki.Launcher\Aki_Data"
 $licenseFile = "..\..\LICENSE.md"
 
 # Delete build folder and contents to ensure it's clean
-if (Test-Path $buildFolder) { Remove-Item -Path $buildFolder -Recurse -Force }
+if (Test-Path "$buildFolder") { Remove-Item -Path "$buildFolder" -Recurse -Force }
 
 # Create build folder and subfolders
-$foldersToCreate = @($buildFolder, $akiDataFolder)
+$foldersToCreate = @("$buildFolder", "$akiDataFolder")
 foreach ($folder in $foldersToCreate) {
-    if (-not (Test-Path $folder)) { New-Item -Path $folder -ItemType Directory }
+    if (-not (Test-Path "$folder")) { New-Item -Path "$folder" -ItemType Directory }
 }
 
 # Move built files to the build folder
 Copy-Item -Path "$launcherExeFolder\Aki.Launcher.exe" -Destination "$buildFolder"
 Copy-Item -Path "$launcherAssetFolder" -Destination "$buildFolder" -Recurse
-# (If any new DLLs need to be copied, add here)
+# If any new DLLs need to be copied, add here
 
-# Write the contents of the license file to a txt in the build folder.
-Get-Content "$licenseFile" | Out-File "$buildFolder\LICENSE-Launcher.txt"
+# Write the contents of the license file to a txt in the build folder
+Get-Content "$licenseFile" | Out-File "$buildFolder\LICENSE-Launcher.txt" -Encoding UTF8
