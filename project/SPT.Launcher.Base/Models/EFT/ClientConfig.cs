6592ec0fc2f8aeb3b7170e1499959b9dfebe0772
CWX <cwxdev@outlook.com>
2024-12-26 22:15:50 +0000

    Fix up ServerConfig, temp workaround

--- project/SPT.Launcher.Base/Models/EFT/ClientConfig.cs
+++ project/SPT.Launcher.Base/Models/EFT/ClientConfig.cs
@@ -12,16 +12,19 @@ namespace SPT.Launcher
     {
         public string BackendUrl;
         public string MatchingVersion;
+        public string Version;
 
         public ClientConfig()
         {
             BackendUrl = "http://127.0.0.1:6969";
+            Version = "live";
             MatchingVersion = "live";
         }
 
         public ClientConfig(string backendUrl)
         {
             BackendUrl = backendUrl;
+            Version = "live";
             MatchingVersion = "live";
         }
     }
