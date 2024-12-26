e040504b01867a059e9a6587add40e0d61785e70
Chomp <dev@dev.sp-tarkov.com>
2024-12-26 21:02:26 +0000

    Updated `Version` to `MatchingVersion`

--- project/SPT.Launcher.Base/Models/EFT/ClientConfig.cs
+++ project/SPT.Launcher.Base/Models/EFT/ClientConfig.cs
@@ -11,18 +11,18 @@ namespace SPT.Launcher
     public class ClientConfig
     {
         public string BackendUrl;
-        public string Version;
+        public string MatchingVersion;
 
         public ClientConfig()
         {
             BackendUrl = "http://127.0.0.1:6969";
-            Version = "live";
+            MatchingVersion = "live";
         }
 
         public ClientConfig(string backendUrl)
         {
             BackendUrl = backendUrl;
-            Version = "live";
+            MatchingVersion = "live";
         }
     }
 }
