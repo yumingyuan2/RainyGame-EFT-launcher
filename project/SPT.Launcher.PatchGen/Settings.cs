14c219c6ae0fa77eeb020103d16bb11111ac09fa
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2025-03-22 13:37:14 -0700

    Add new patch gen project based around calling hdiffz.exe
    (cherry picked from commit b53ccf191203aebc6bd991d9f99bd94500a58a6b)

---
+++ project/SPT.Launcher.PatchGen/Settings.cs
@@ -0,0 +1,41 @@
+﻿using System.Configuration;
+
+namespace SPT.Launcher.PatchGen
+{
+    public static class Settings
+    {
+
+        public static string Read(string key, string defaultValue = "")
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                return appSettings[key] ?? defaultValue;
+            }
+            catch (ConfigurationErrorsException)
+            {
+                return defaultValue;
+            }
+        }
+
+        public static void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException) { }
+        }
+    }
+}
