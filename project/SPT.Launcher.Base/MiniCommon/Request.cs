b9235744fac836c08ec5814071335ea148d03394
Archangel <jesse@archangel.wtf>
2025-06-24 15:55:21 +0200

    Disable proxies on HttpClient

--- project/SPT.Launcher.Base/MiniCommon/Request.cs
+++ project/SPT.Launcher.Base/MiniCommon/Request.cs
@@ -23,7 +23,8 @@ namespace SPT.Launcher.MiniCommon
 
         private static readonly HttpClientHandler httpClientHandler = new()
         {
-            ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+            ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true,
+            UseProxy = false
         };
 
         private static readonly HttpClient _httpClient = new(httpClientHandler);
